96bf3c8e7c5c55c987f54a3d3660296b2b87b905
Simon Giroux <simon.giroux.portelance@gmail.com>
2026-04-27 21:14:11 -0400

    hero sprite and dungeon

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [heroSpriteCol, setHeroSpriteCol] = useState(0)
+  const [heroState, setHeroState] = useState("rightIdleNoWeapon")
+  const [worldPos, setWorldPos] = useState({ x: -24, y: 43});
+  const viewPortSize = 64;
+  const ANIMATIONSPEED = 120; // milliseconds (animation speed)
+  const WALKINGSPEED=5
+
+
+  const heroStateInfo={rightIdleNoWeapon:{frameCount:12, row:0},rightIdleWeapon:{frameCount:12, row:-1}, rightWalkNoWeapon:{frameCount:8, row:-2},rightWalkWeapon:{frameCount:8, row:-3},rightAttackNoWeapon:{frameCount:7, row:-4},rightAttackWeapon:{frameCount:6, row:-5},downIdleNoWeapon:{frameCount:12, row:-6},downIdleWeapon:{frameCount:12, row:-7}, downWalkNoWeapon:{frameCount:8, row:-8},downWalkWeapon:{frameCount:8, row:-9},downAttackNoWeapon:{frameCount:7, row:-10},downAttackWeapon:{frameCount:6, row:-11}, leftIdleNoWeapon:{frameCount:12, row:-12},leftIdleWeapon:{frameCount:12, row:-13}, leftWalkNoWeapon:{frameCount:8, row:-14},leftWalkWeapon:{frameCount:8, row:-15},leftAttackNoWeapon:{frameCount:7, row:-16},leftAttackWeapon:{frameCount:6, row:-17}, upIdleNoWeapon:{frameCount:12, row:-18},upIdleWeapon:{frameCount:12, row:-19}, upWalkNoWeapon:{frameCount:8, row:-20},upWalkWeapon:{frameCount:8, row:-21},upAttackNoWeapon:{frameCount:7, row:-22},upAttackWeapon:{frameCount:6, row:-23}, dead:{frameCount:14, row:-24}};
+
+  const keyMap = {
+  ArrowDown: "downWalkNoWeapon",
+  ArrowUp: "upWalkNoWeapon",
+  ArrowLeft: "leftWalkNoWeapon",
+  ArrowRight: "rightWalkNoWeapon",
+};
+
+  useEffect(() => {
+const interval = setInterval(() => {
+  setHeroSpriteCol((prev) => (prev - 1) % heroStateInfo[heroState].frameCount);
+}, ANIMATIONSPEED);
+
+  return () => clearInterval(interval);
+}, [heroState]);
+
+useEffect(() => {
+
+const handleKeyDown = (e) => {
+
+  setWorldPos((prev) => {
+    let newPos = { ...prev };
+
+    if (e.key === "ArrowUp") {
+      newPos.y += WALKINGSPEED;
+      setHeroState("upWalkNoWeapon");
+    }
+
+    if (e.key === "ArrowDown") {
+      newPos.y -= WALKINGSPEED;
+      setHeroState("downWalkNoWeapon");
+    }
+
+    if (e.key === "ArrowLeft") {
+      newPos.x += WALKINGSPEED;
+      setHeroState("leftWalkNoWeapon");
+    }
+
+    if (e.key === "ArrowRight") {
+      newPos.x -= WALKINGSPEED;
+      setHeroState("rightWalkNoWeapon");
+    }
+
+    return newPos;
+  });
+};
+
+window.addEventListener("keydown", handleKeyDown);
+window.addEventListener("keyup", handleKeyDown);
+window.addEventListener("ArrowRight", handleKeyDown);
+window.addEventListener("ArrowLeft", handleKeyDown);
+
+      return () => {
+window.addEventListener("keydown", handleKeyDown);
+window.addEventListener("keyup", handleKeyDown);
+window.addEventListener("ArrowRight", handleKeyDown);
+window.addEventListener("ArrowLeft", handleKeyDown);
+    };
+}, []);
+
+  return (
+    <>
+<div
+  style={{
+    position: "fixed",
+    inset: 0,
+    overflow: "hidden",
+  }}
+>
+  {/* 🌍 MAP (moves) */}
+  <div
+    style={{
+      position: "absolute",
+      width: "100vw",
+      height: "100vh",
+      backgroundImage: "url('/dungeon.jfif')",
+       transform: "scale(1.5)",
+      backgroundPosition: "center",
+      transform: `translate(${worldPos.x}px, ${worldPos.y}px)`,
+    }}
+  />
+  <div
+    style={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: viewPortSize,
+      height: viewPortSize,
+      backgroundImage: "url('/warrior.png')",
+      backgroundPosition: `${heroSpriteCol * viewPortSize}px ${
+        heroStateInfo[heroState].row * viewPortSize
+      }px`,
+      backgroundRepeat: "no-repeat",
+    }}
+  />
+</div>
+    </>
+  )
+}
+
+export default App
